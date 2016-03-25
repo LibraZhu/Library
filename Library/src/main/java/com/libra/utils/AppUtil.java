9e7cbf400eae3fd801e9cb5e3fd4f04d895ed3bd
letin <letin.zhu@gmail.com>
2016-03-25 08:59:23 +0800

    0.0.3---添加bottom navigation bar

--- Library/src/main/java/com/libra/utils/AppUtil.java
+++ Library/src/main/java/com/libra/utils/AppUtil.java
@@ -523,13 +523,13 @@ public class AppUtil {
     }
 
 
-    public int dip2px(Context context, float dipValue) {
+    public static int dip2px(Context context, float dipValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dipValue * scale + 0.5f);
     }
 
 
-    public int px2dip(Context context, float pxValue) {
+    public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
